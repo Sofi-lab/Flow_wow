3924c3344337d397fd2f3640d6bad586c1f64dbb
Sofi <sofa.kirikova@yandex.ru>
2025-09-13 13:27:37 +0300

    FEAT: Добавлена логика получения валюты, бэкап в json формате и запись данных в .csv

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import requests
+import pandas as pd
+import json
 
-def print_hi(name):
-    print(f'Hi, {name}')
+
+api_key = "87fbf054dc51175d0b1e67bd"
+url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD"
 
 
+response = requests.get(url)
+result = response.json()
 
-if __name__ == '__main__':
+# Бэкап в json формате
+with open("currency.json", "w") as f:
+    json.dump(result, f, indent=2)
 
-    print_hi('PyCharm')
+currency = result['conversion_rates'] #Получили словарь с валютой
+
+df = pd.DataFrame(list(currency.items()), columns = ['Currency', 'Rate_to_USD']) #Преобразуем в DF
+
+df.to_csv("currencyes.csv", index=False)
 
